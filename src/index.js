4d04b29f45555b59cb2b21279f7db44e82d716f8
IQDUKE <www_root@hotmail.com>
2024-08-26 17:13:33 +0800

    Update index.js
    update domain fakehistory.top

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.fakehistory.top": dockerHub,
+  "quay.fakehistory.top": "https://quay.io",
+  "gcr.fakehistory.top": "https://gcr.io",
+  "k8s-gcr.fakehistory.top": "https://k8s.gcr.io",
+  "k8s.fakehistory.top": "https://registry.k8s.io",
+  "ghcr.fakehistory.top": "https://ghcr.io",
+  "cloudsmith.fakehistory.top": "https://docker.cloudsmith.io",
+  "ecr.fakehistory.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.fakehistory.top": dockerHub,
 };
 
 function routeByHosts(host) {
